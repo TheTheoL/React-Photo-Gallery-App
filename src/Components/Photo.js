a896ae8ae87519981ff86eb05b47728186fe756e
Theo L <atlufkin@outlook.com>
2022-02-01 13:44:14 -0500

    Update Photo.js

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -13,7 +13,7 @@ export default class Photo extends Component {
               
               <li className="not-found">
                 <h3>No Results Found</h3>
-                <p>You search did not return any results. Please try again.</p>
+                <p>Your search did not return any results. Please try again.</p>
               </li>
             </ul>
           </div>
